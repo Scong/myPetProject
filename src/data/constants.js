e09a76dd0cf6eb4120a1f4b3cbcfd55577908c6b
scong <stcongleton@gmail.com>
2019-09-30 00:24:40 -0400

    Generated dataset.

---
+++ src/data/constants.js
@@ -0,0 +1,24 @@
+const AnimalClassifications = [
+  ["Mammal", "Cat", "American Bobtail Cat"],
+  ["Mammal", "Cat", "Abyssinian Cat"],
+  ["Mammal", "Dog", "Golden Retriever"],
+  ["Mammal", "Dog", "Finnish Lapphund"],
+  ["Mammal", "Dog", "German Shorthaired Pointer"],
+  ["Amphibian", "Frog", "African claws frog"],
+  ["Amphibian", "Frog", "Poison dart frog"],
+  ["Amphibian", "Frog", "Red-eyed tree frog"],
+  ["Reptile", "Lizard", "Australian Water Dragon"],
+  ["Reptile", "Lizard", "Beaded Lizard"],
+  ["Reptile", "Lizard", "Bearded Dragon"],
+  ["Reptile", "Lizard", "Bibron's Gecko"],
+  ["Reptile", "Lizard", "Black and White Tegu"],
+  ["Bird", "Cockatiels", "Lutino Cockatiel"],
+  ["Bird", "Cockatiels", "Pearl Cockatiel"],
+  ["Bird", "Cockatiels", "Pied Cockatiel"],
+  ["Bird", "Cockatoos", "Umbrella cockatoo"],
+  ["Bird", "Parrots", "African Grey Parrots"],
+]
+
+module.exports = {
+  AnimalClassifications
+}
---
+++ src/data/seed.js
@@ -0,0 +1,69 @@
+const faker = require('faker')
+const {AnimalClassifications} = require("./constants")
+const fs = require('fs');
+
+const NUM_PETS = 1000
+const NUM_OWNERS = 30
+let owners = []
+let animals = []
+
+const genId = () => {
+  return '_' + Math.random().toString(36).substr(2, 9)
+}
+
+const fillOwners = () => {
+  for( let i = 0; i < NUM_OWNERS; i++) {
+    owners.push({
+      id: genId(),
+      firstName: faker.name.firstName(),
+      jobTitle: faker.name.jobTitle(),
+      lastName: faker.name.lastName()
+    })
+  }
+}
+
+const fillAnimals = () => {
+  const ownerIds = owners.map(({id}) => id)
+  for( let i = 0; i < NUM_PETS; i++) {
+    animals.push({
+      id: genId(),
+      name: faker.name.firstName(),
+      classification: faker.random.arrayElement(AnimalClassifications),
+      age: faker.random.number(),
+      owner_id: faker.random.arrayElement(ownerIds.concat([undefined, undefined, undefined]))
+    })
+  }
+}
+
+const createDataFiles = () => {
+  fs.writeFile(__dirname + '/animals.json', JSON.stringify(animals, null, 2), (err) => {
+    // throws an error, you could also catch it here
+    if (err) throw err;
+
+    // success case, the file was saved
+    console.log('Animals saved!');
+  })
+
+  fs.writeFile(__dirname + '/owners.json', JSON.stringify(owners, null, 2), (err) => {
+    // throws an error, you could also catch it here
+    if (err) throw err;
+
+    // success case, the file was saved
+    console.log('Owners saved!');
+  })
+}
+
+const seed = () => {
+  fillOwners()
+  fillAnimals()
+  createDataFiles()
+}
+
+seed()
+
+
+
+
+
+
+
